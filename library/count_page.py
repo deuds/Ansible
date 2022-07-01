e500050e607595a8f3276dfdc5993d8e70026315
deuds <82383143+deuds@users.noreply.github.com>
2022-07-01 09:45:22 +0200

    Update count_page.py

--- library/count_page.py
+++ library/count_page.py
@@ -7,13 +7,13 @@ import MySQLdb
 def main(): 
     module = AnsibleModule( 
         argument_spec=dict( 
-        db_name    = dict(required=True, type=’str’), 
-        request    = dict(required=True, type=’str’), 
+        db_name    = dict(required=True, type='str'), 
+        request    = dict(required=True, type='str'),
         ) 
     )
-    
-    db_name_local = module.params.get(’db_name’) 
-    request_local = module.params.get(’request’)
+
+    db_name_local = module.params.get('db_name') 
+    request_local = module.params.get('request')
 
     db = MySQLdb.connect(db=db_name_local) 
     cur = db.cursor() 
